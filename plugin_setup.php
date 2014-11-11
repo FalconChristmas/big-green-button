968484b8013f26582c2c93d14b3b1fe786d2e57e
Mathew Mrosko <falconpiplayer@matmrosko.com>
2014-11-10 21:40:12 -0800

    Fix the setting helper function that was changed in the core helper code.

--- plugin_setup.php
+++ plugin_setup.php
@@ -22,7 +22,7 @@ function StartPlaylist()
 <fieldset>
 <legend>Big Green Button</legend>
 
-<p>Playlist: <?php PrintSettingSelect("Playlist", "PlaylistSelect", "disabled", $playlists, "big-green-button"); ?> Repeat: <?php PrintSettingCheckbox("Repeat", "Repeat", "1", "0", "big-green-button"); ?></p>
+<p>Playlist: <?php PrintSettingSelect("Playlist", "PlaylistSelect", "0", "0", "disabled", $playlists, "big-green-button"); ?> Repeat: <?php PrintSettingCheckbox("Repeat", "Repeat", "1", "0", "big-green-button"); ?></p>
 
 <p><input class="button" onClick="StartPlaylist();" type="submit" value="Start" /></p>
 
